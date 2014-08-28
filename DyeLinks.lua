f08fd29d94b67558c3fd665abd66198395c66568
Randactyl <rafowler@randactyl.com>
2014-08-28 12:20:10 -0700

    Initial commit. Addon version 1.1

---
+++ DyeLinks.lua
@@ -0,0 +1,21 @@
+function ZO_Dyeing:AttemptExit(exitingToAchievementId)
+    if exitingToAchievementId then
+        ACHIEVEMENTS.popup:Hide()
+
+        local link = ZO_LinkHandler_CreateChatLink(GetAchievementLink, exitingToAchievementId)
+        local name, linkStyle, linkType, id, progress, timestamp = ZO_LinkHandler_ParseLink(link)
+        
+        ACHIEVEMENTS:ShowAchievementPopup(exitingToAchievementId, progress, timestamp)
+        ZO_PopupTooltip_Hide()
+    else
+        ACHIEVEMENTS.popup:Hide()
+
+        if self:AreTherePendingDyes() then
+            if self:AreAllItemsBound() then
+                   ZO_Dialogs_ShowDialog("EXIT_DYE_UI")
+            end
+        else
+            self:ConfirmExit()
+        end
+    end
+end
