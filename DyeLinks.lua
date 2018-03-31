ea09d879ded69adfeca7e64787771919d6156d6f
Randactyl <rafowler@randactyl.com>
2018-03-31 10:49:49 -0700

    update to newest ZOS code

--- DyeLinks.lua
+++ DyeLinks.lua
@@ -1,26 +1,35 @@
---overrides function ZO_Restyle_Keyboard:AttemptExit(exitingToAchievementId) in restyle_keyboard.lua
-function ZO_RESTYLE_KEYBOARD.AttemptExit(self, exitingToAchievementId)
-    self.exitingToAchievementId = exitingToAchievementId
+local ACHIEVEMENTS_KB = SYSTEMS.systems.achievements.keyboardObject
+local RESTYLE_KB = SYSTEMS.systems.restyle.keyboardObject
 
-    if exitingToAchievementId then
-        ACHIEVEMENTS.popup:Hide()
+-- Overrides function ZO_RestyleStation_Keyboard:AttemptExit(exitDestinationData) in restylestation_keyboard.lua
+function RESTYLE_KB.AttemptExit(self, exitDestinationData)
+    if exitDestinationData and exitDestinationData.achievementId then
+        ACHIEVEMENTS_KB.popup:Hide()
 
-        local link = ZO_LinkHandler_CreateChatLink(GetAchievementLink, exitingToAchievementId)
+        local link = ZO_LinkHandler_CreateChatLink(GetAchievementLink, exitDestinationData.achievementId)
         local name, linkStyle, linkType, id, progress, timestamp = ZO_LinkHandler_ParseLink(link)
 
-        ACHIEVEMENTS:ShowAchievementPopup(exitingToAchievementId, progress, timestamp)
+        ACHIEVEMENTS_KB:ShowAchievementPopup(exitDestinationData.achievementId, progress, timestamp)
         ZO_PopupTooltip_Hide()
     else
-        ACHIEVEMENTS.popup:Hide()
+        ACHIEVEMENTS_KB.popup:Hide()
 
-        if ZO_Dyeing_AreTherePendingDyes(self.mode) then
-            if ZO_Dyeing_AreAllItemsBound(self.mode) then
-                ZO_Dialogs_ShowDialog("EXIT_DYE_UI")
-            else
-                ZO_Dialogs_ShowDialog("EXIT_DYE_UI_BIND")
+        -- Begin ZOS code
+        self.exitDestinationData = exitDestinationData
+        local preservePendingChanges = exitDestinationData and exitDestinationData.preservePendingChanges or false
+
+        if ZO_RESTYLE_SHEET_WINDOW_KEYBOARD:AreChangesPending() and not preservePendingChanges then
+            local function Confirm()
+                self.ConfirmExit()
             end
+
+            local function Decline()
+                self.exitDestinationData = nil
+            end
+            ZO_RESTYLE_SHEET_WINDOW_KEYBOARD:ShowRevertRestyleChangesDialog("CONFIRM_REVERT_RESTYLE_CHANGES", Confirm, Decline)
         else
             self:ConfirmExit()
         end
+        -- End ZOS code
     end
 end
